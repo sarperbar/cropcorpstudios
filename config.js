df5bc5e65574f51c9e09dcd4bd8c0aa349440ee7
Sarper ERBAR <113534924+sarperbar@users.noreply.github.com>
2026-02-03 14:54:21 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+
+/* Crop Corp. Studios - Config */
+const studioConfig = {
+    // Sosyal Medya ve İletişim
+    social: {
+        youtube: "https://www.youtube.com/@CropCorp.Studios",
+        email: "cropcorpstudios@gmail.com",
+        twitter: "#",   
+        linkedin: "https://www.linkedin.com/company/crop-corp-studios"   
+    },
+    
+    // Oyun Linkleri
+    cropCollection: {
+        googlePlay: "#",
+        appStore: "#"
+    }
+};
